c5bf04291e06bd2cedc5bde30c33227edd03706a
Afraz ul haque <75325263+afraz-rupak@users.noreply.github.com>
2021-12-09 14:47:28 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'a71619e7-1111-411a-9511-db0f302f8b08';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+
+export default App;
